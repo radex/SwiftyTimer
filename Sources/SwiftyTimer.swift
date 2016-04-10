7d8806e1c4854b876444169890710d09112a1043
radex <radexpl@gmail.com>
2016-04-10 14:51:50 +0200

    Improve documentation

--- Sources/SwiftyTimer.swift
+++ Sources/SwiftyTimer.swift
@@ -25,45 +25,9 @@
 import Foundation
 
 extension NSTimer {
-    /// Create a timer that will call `block` once after the specified time.
-    ///
-    /// - Note: The timer won't fire until it's scheduled on the run loop.
-    ///         Use `NSTimer.after` to create and schedule a timer in one step.
-    /// - Note: The `new` class function is a workaround for a crashing bug when using convenience initializers (rdar://18720947)
-
-    public class func new(after interval: NSTimeInterval, _ block: () -> Void) -> NSTimer {
-        return CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent() + interval, 0, 0, 0) { _ in
-            block()
-        }
-    }
     
-    /// Create a timer that will call `block` repeatedly in specified time intervals.
-    ///
-    /// - Note: The timer won't fire until it's scheduled on the run loop.
-    ///         Use `NSTimer.after` to create and schedule a timer in one step.
-    /// - Note: The `new` class function is a workaround for a crashing bug when using convenience initializers (rdar://18720947)
-
-    public class func new(every interval: NSTimeInterval, _ block: () -> Void) -> NSTimer {
-        return CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent() + interval, interval, 0, 0) { _ in
-            block()
-        }
-    }
+// MARK: Schedule timers
     
-    /// Create a timer that will call `block` repeatedly in specified time intervals.
-    /// (This variant also passes the timer instance to the block)
-    ///
-    /// - Note: The timer won't fire until it's scheduled on the run loop.
-    ///         Use `NSTimer.after` to create and schedule a timer in one step.
-    /// - Note: The `new` class function is a workaround for a crashing bug when using convenience initializers (rdar://18720947)
-    
-    @nonobjc public class func new(every interval: NSTimeInterval, _ block: NSTimer -> Void) -> NSTimer {
-        var timer: NSTimer!
-        timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent() + interval, interval, 0, 0) { _ in
-            block(timer)
-        }
-        return timer
-    }
-
     /// Create and schedule a timer that will call `block` once after the specified time.
     
     public class func after(interval: NSTimeInterval, _ block: () -> Void) -> NSTimer {
@@ -88,6 +52,49 @@ extension NSTimer {
         timer.start()
         return timer
     }
+    
+// MARK: Create timers without scheduling
+    
+    /// Create a timer that will call `block` once after the specified time.
+    ///
+    /// - Note: The timer won't fire until it's scheduled on the run loop.
+    ///         Use `NSTimer.after` to create and schedule a timer in one step.
+    /// - Note: The `new` class function is a workaround for a crashing bug when using convenience initializers (rdar://18720947)
+
+    public class func new(after interval: NSTimeInterval, _ block: () -> Void) -> NSTimer {
+        return CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent() + interval, 0, 0, 0) { _ in
+            block()
+        }
+    }
+    
+    /// Create a timer that will call `block` repeatedly in specified time intervals.
+    ///
+    /// - Note: The timer won't fire until it's scheduled on the run loop.
+    ///         Use `NSTimer.every` to create and schedule a timer in one step.
+    /// - Note: The `new` class function is a workaround for a crashing bug when using convenience initializers (rdar://18720947)
+
+    public class func new(every interval: NSTimeInterval, _ block: () -> Void) -> NSTimer {
+        return CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent() + interval, interval, 0, 0) { _ in
+            block()
+        }
+    }
+    
+    /// Create a timer that will call `block` repeatedly in specified time intervals.
+    /// (This variant also passes the timer instance to the block)
+    ///
+    /// - Note: The timer won't fire until it's scheduled on the run loop.
+    ///         Use `NSTimer.every` to create and schedule a timer in one step.
+    /// - Note: The `new` class function is a workaround for a crashing bug when using convenience initializers (rdar://18720947)
+    
+    @nonobjc public class func new(every interval: NSTimeInterval, _ block: NSTimer -> Void) -> NSTimer {
+        var timer: NSTimer!
+        timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent() + interval, interval, 0, 0) { _ in
+            block(timer)
+        }
+        return timer
+    }
+    
+// MARK: Manual scheduling
     
     /// Schedule this timer on the run loop
     ///
